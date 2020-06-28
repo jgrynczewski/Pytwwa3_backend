dd858b3b3a9a2aa088b3579f1fbb018f63aa9653
Jerzy <jgrynczewski@gmail.com>
2020-06-28 10:38:56 +0200

    Django intro part2

--- intro_django/hello/urls.py
+++ intro_django/hello/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("foo", views.hello)
+    path("foo", views.hello),
+    path("bar", views.drugi),
+    path("buz", views.trzeci)
 ]
--- intro_django/hello/views.py
+++ intro_django/hello/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def hello(request):
     return HttpResponse("Witaj świecie!")
+
+
+def drugi(request):
+    msg = "<!DOCTYPE html><html><head><title>Drugi widok</title></head><body>To jest drugi widok <p>Nowa linia</p></body></html>"
+    return HttpResponse(msg)
+
+
+def trzeci(request):
+    return render(request, 'hello/trzeci.html')
