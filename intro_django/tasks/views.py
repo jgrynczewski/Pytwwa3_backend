18e81a9222fb5a7c73e72bdf7a9bfb8e7c56cf90
Jerzy <jgrynczewski@gmail.com>
2020-06-28 14:15:36 +0200

    Django intro part4

--- intro_django/tasks/views.py
+++ intro_django/tasks/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 def index(request):
     t = Task.objects.all()
-    return render(request, 'tasks/task.html', {'tasks': t})
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['text']
+            task = Task(text=text)
+            task.save()
+    else:
+        form = TaskForm()
+
+    return render(request, 'tasks/task.html', {'tasks': t, "form": form})
