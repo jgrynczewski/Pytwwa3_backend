e1bf0bcdc5e50f26b88c103359eb331148cca233
Jerzy <jgrynczewski@gmail.com>
2020-07-04 15:18:50 +0200

    Flask intro - templates, jinja part2

---
+++ flask_intro/4_conditions/app.py
@@ -0,0 +1,23 @@
+import datetime
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    now = datetime.datetime.now()
+    if (now.month == 1 and now.day == 1):
+        var="Tak"
+    else:
+        var="Nie"
+    return render_template("index.html", context=var)
+
+
+@app.route("/newyear")
+def index2():
+    now = datetime.datetime.now()
+    var=now.month == 1 and now.day == 1
+    return render_template("index2.html", context=var)
