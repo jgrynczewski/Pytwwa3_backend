4cb878294823b181d225cd931c6436f1dd6ad4a1
Jerzy <jgrynczewski@gmail.com>
2020-07-11 13:24:39 +0200

    Flask intro - formularz part 3

--- flask_intro/10_tasks/app.py
+++ flask_intro/10_tasks/app.py
@@ -1,14 +1,24 @@
+import os
+
 from flask import Flask
 from flask import render_template
 from flask import request
 
 app = Flask(__name__)
 
-tasks = []
+if os.path.isfile("tasks.txt"):
+    with open("tasks.txt", 'r') as a_file:
+        tasks = a_file.readlines()
+else:
+    with open("tasks.txt", 'w+') as a_file:
+        tasks = []
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        tasks.append(request.form.get("task"))
+        task = request.form.get("task")
+        with open("tasks.txt", "a") as a_file:
+            a_file.write(f"{task}\n")
 
+        tasks.append(task)
     return render_template('index.html', tasks=tasks)
