9456162463eb11defba092f4018cc0c3ea079042
Jerzy <jgrynczewski@gmail.com>
2020-08-01 15:38:57 +0200

    Requests

---
+++ flask_intro/14_api/1.py
@@ -0,0 +1,17 @@
+# Comand-line interface application
+import requests
+
+other = input("Podaj walutę: ")
+response = requests.get("http://data.fixer.io/api/latest",
+                        params= {'access_key':'032053b70cf616de08638aeaeb1cfd1d',
+                                'symbols': other.upper()}
+                        )
+if response.status_code != 200:
+    raise Exception("Error: Api request unsuccessful")
+
+data = response.json()
+if data.get('rates'):
+    res = data.get('rates').get(other.upper())
+    print(f"1 {other.upper()} to {res} EURO")
+else:
+    print("Nie rozpoznano waluty.")
