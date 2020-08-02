7148442e308128965c056bca28b60df1a1639011
Jerzy <jgrynczewski@gmail.com>
2020-08-02 13:40:43 +0200

    ajax part 2 fix

--- flask_intro/16_api/static/index.js
+++ flask_intro/16_api/static/index.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('form').onsubmit = function() {
 
         // 1. Inicjalizacja zapytania asycnchronicznego (XMLHTTP request)
-        const request = new XMLHttpRequest();
-        request.open('POST', '/convert');
+        const async_request = new XMLHttpRequest();
+        async_request.open('POST', '/convert');
         // Przygotowanie danych do wysyłki
         const amount = document.querySelector('#amount').value;
         const symbols = document.querySelector("#symbols").value;
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', function() {
         data.append('symbols', symbols);
 
         // 2. Co ma się stać po otrzymaniu odpowiedzi
-        request.onload = function() {
-            const response_data = request.responseText;
+        async_request.onload = function() {
+            const response_data = async_request.responseText;
             const response = JSON.parse(response_data);
 
             if (response.success) {
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
 
-        request.send(data);
+        async_request.send(data);
         return false;
     }
 });
