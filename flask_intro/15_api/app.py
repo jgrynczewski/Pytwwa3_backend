f6b6c7bf60a5489d9d8a75e6dfb838ba959fb4ab
Jerzy <jgrynczewski@gmail.com>
2020-08-02 10:32:14 +0200

    api consuming part2

---
+++ flask_intro/15_api/app.py
@@ -0,0 +1,39 @@
+# Web application
+import requests #urllib
+
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/convert", methods=['POST'])
+def convert():
+
+    amount = request.form.get('amount')
+    symbols = request.form.get('symbols').upper()
+
+    response  = requests.get("http://data.fixer.io/api/latest",
+                        params = {'access_key':'032053b70cf616de08638aeaeb1cfd1d',
+                                'symbols': symbols}
+                 )
+
+    if response.status_code != 200:
+        return render_template("index.html", info=f"Niepowowdznie. Kod HTTP {response.status_code}")
+
+    res_dict = response.json()
+
+    if not res_dict.get('success'):
+        return render_template("index.html", info = f"Error: {res_dict.get('error').get('info')}")
+
+    converter = res_dict.get('rates').get(symbols)
+
+    result = float(amount) / converter
+
+    return render_template("index.html", info=f"Wypłacono {round(result, 2)} euro.")
