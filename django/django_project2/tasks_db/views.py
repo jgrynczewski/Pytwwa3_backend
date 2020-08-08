7bc046bac1deca2c9ea1d4bb9706dc7d228a6c80
Jerzy <jgrynczewski@gmail.com>
2020-08-08 14:45:04 +0200

    django part 5a

--- django/django_project2/tasks_db/views.py
+++ django/django_project2/tasks_db/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 
-tasks = []
+from .models import Task
+
 
 # Create your views here.
 def index(request):
+    tasks = Task.objects.all()
+
     return render(request, 'tasks_db/index.html', {
         "tasks": tasks
     })
 
 
+@require_http_methods("[POST]")
 def register(request):
-    t = request.GET['task']
-    tasks.append(t)
+    t = request.POST['task']
+
+    # C (CREATE - insert) z CRUD
+    task = Task(text=t)
+    task.save()
+
+    # R (READ - select) z CRUD
+    tasks = Task.objects.all()
+    # objects - menadżer zapytań
+    # metody all(), first(), filter(), ... tego obiektu zwracają obiekt klasy QuerySet
+
     return render(request, 'tasks_db/index.html', {
         "tasks": tasks
     })
