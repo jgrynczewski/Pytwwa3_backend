f25bd5f79024917e8a461b153e585d4c3b6292c5
Jerzy <jgrynczewski@gmail.com>
2020-08-22 11:00:15 +0200

    Django - class-based view part2

--- django/django_project2/class_based_views/urls.py
+++ django/django_project2/class_based_views/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('hello', views.hello, name="hello"),
     path('hello2', views.HelloView.as_view(), name='hello2'),
 
-    path('person/<int:id>', views.person_detail, name='person_detail')
+    path('person/<int:id>', views.person_detail, name='person_detail'),
+    path('person2/<int:id>', views.PersonView.as_view(), name='person_detail2'),
+    path('person3/<int:pk>', views.PersonDetailView.as_view(), name='person_detail3'),
+
+    path('create', views.create_person, name='create_person')
 ]
